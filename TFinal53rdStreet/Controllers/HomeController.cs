dcd09793ebc0e170590738569bc1766a15d8fd58
Lenovo <Lenovo@DESKTOP-1SABM7D>
2018-06-06 16:17:19 +0100

    Alteração do nome do controller acedido por defeito

--- TFinal53rdStreet/Controllers/HomeController.cs
+++ TFinal53rdStreet/Controllers/HomeController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TFinal53rdStreet.Models;
 
 namespace TFinal53rdStreet.Controllers
 {
     public class HomeController : Controller
     {
+        private MusicalDB db = new MusicalDB();
         public ActionResult Index()
         {
+            var MusicalList = db.Musical.ToList().OrderBy(m => m.Title);
             return View();
         }
 
